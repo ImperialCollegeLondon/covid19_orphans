d1f930695bc55f35cb126805d8f5429909822b48
Ettie Unwin <hunwin@imperial.ac.uk>
2021-11-09 09:54:34 +1300

    Update

--- global_age_analysis_2021_run.R
+++ global_age_analysis_2021_run.R
@@ -7,6 +7,7 @@ source("global_age_analysis_2021/R/run_global_extrapolation_europe_april.R")
 source("global_age_analysis_2021/R/format_update_results.R")
 format_table("apr")
 
+# Clears the environment
 rm(list = ls())
 
 # Updates global numbers though 31st October
