7d6d99d1d190dd26ca34e354ff0499249fb3c580
Ettie Unwin <hunwin@imperial.ac.uk>
2021-11-10 16:16:20 +1300

    Short run with numbers

--- global_age_analysis_2021_run.R
+++ global_age_analysis_2021_run.R
@@ -15,13 +15,13 @@ format_table("oct")
 combine_table()
 
 # Runs age analysis
-# source("global_age_analysis_2021/R/run_age_analysis_uncertainty.R")
+source("global_age_analysis_2021/R/run_age_analysis_uncertainty.R")
 
-# # Run global age model
-# source("global_age_analysis_2021/R/global_age_model.R")
-# 
-# # Makes time series plot
-# source("global_age_analysis_2021/R/orphanhood_time_series.R")
-#  
-# # Makes figures and tables
-# source("global_age_analysis_2021/R/age_plots.R")
+# Run global age model
+source("global_age_analysis_2021/R/global_age_model.R")
+
+# Makes time series plot
+source("global_age_analysis_2021/R/orphanhood_time_series.R")
+
+# Makes figures and tables
+source("global_age_analysis_2021/R/age_plots.R")
