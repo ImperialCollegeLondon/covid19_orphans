58cd21103e96da3408d7a9febb6be3351fa2a381
Ettie Unwin <hunwin@imperial.ac.uk>
2021-12-13 16:10:43 +1300

    Update model

--- global_age_analysis_2021/R/brms_joint_fit.R
+++ global_age_analysis_2021/R/brms_joint_fit.R
@@ -2,10 +2,11 @@
 joint_fit <- function(all_data, data, formula, plot = FALSE, loo = FALSE){
   # Fit model
   set.seed(1)
-  mod_full <- brm(formula, data = data, 
+  mod_full <- brm(formula, data = data, seed = 2,
                   control = list(max_treedepth = 16),
-                  family = multinomial(), seed = 1, iter = 50, 
-                  cores = 2, file_refit = "on_change", 
+                  family = multinomial(), 
+                  cores = 4,
+                  file_refit = "always", 
                   file = "global_age_analysis_2021/data/age_outputs/mod_fit.rds")
   saveRDS(mod_full, "global_age_analysis_2021/data/age_outputs/global_age_fit.RDS")
   # Format data for analysis
